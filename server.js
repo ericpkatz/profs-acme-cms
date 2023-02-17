509298e7d22f9a0e2087c2b032a92b45dacec4f6
ericpkatz <ericpkatz@gmail.com>
2023-02-16 19:57:47 -0500

    commit

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const express = require('express');
-const app = express();
 const Sequelize = require('sequelize');
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_cms_db');
 
@@ -14,6 +12,82 @@ const Page = conn.define('page', {
 });
 
 Page.belongsTo(Page, { as: 'parent'});
+Page.hasMany(Page, { foreignKey: 'parentId', as: 'children'});
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/pages/:id', async(req, res, next)=> {
+  try {
+    const page = await Page.findByPk(
+      req.params.id,
+      {
+        include: [
+          {
+            model: Page,
+            as: 'parent'
+          },
+          {
+            model: Page,
+            as: 'children'
+          }
+        ]
+      }
+    );
+    res.send(page);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+app.get('/api/pages', async(req, res, next)=> {
+  try {
+    res.send(await Page.findAll({
+      include: [
+        {
+          model: Page,
+          as: 'parent'
+        }
+      ]
+    }));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.post('/api/pages', async(req, res, next)=> {
+  try {
+    const page = await Page.create(req.body);
+    res.status(201).send(page);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.delete('/api/pages/:id', async(req, res, next)=> {
+  try {
+    const page = await Page.findByPk(req.params.id);
+    await page.destroy();
+    res.sendStatus(204);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.put('/api/pages/:id', async(req, res, next)=> {
+  try {
+    const page = await Page.findByPk(req.params.id);
+    await page.update(req.body);
+    res.send(page);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
 
 
 const port = process.env.PORT || 3000;
